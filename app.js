07d58346569a78abb7259d98ee1647d8a176b617
Aniket-Patidar <aniketpatidar60@gmail.com>
2024-02-28 17:16:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,14 +15,15 @@ const fileupload = require('express-fileupload');
 app.use(fileupload());
 
 //cors setup
-// app.use(cors());
+
 app.use(
-	cors({
-		credentials: true,
-		origin: true || 'http://localhost:8080/',
-		methods: ['GET', 'POST, PUT', 'DELETE'],
-	})
+    cors({
+        origin: "*",
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    })
 );
+
 
 //logger
 app.use(loggger('tiny'));
