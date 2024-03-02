9b1891d334d63925183c822e9818ae7c53994d98
Aniket-Patidar <aniketpatidar60@gmail.com>
2024-03-02 09:40:07 +0530

    update secqure  feature

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,15 +3,17 @@ const ErrorHandler = require('../utils/ErrorHandlers');
 const { catchAsyncError } = require('./catchAsyncError');
 
 exports.isAuthenticated = catchAsyncError(async (req, res, next) => {
-	const { token } = req.cookies;
+    const { token } = req.cookies;
 
-	if (!token) {
-		return next(new ErrorHandler('Please Login to access this resource', 401));
-	}
+    if (!token) {
+        return next(new ErrorHandler('Please Login to access this resource', 401));
+    }
 
-	const { id } = jwt.verify(token, process.env.JWT_TOKEN_SECRET);
-	req.id = id;
-
-    next();
-
+    try {
+        const { id } = jwt.verify(token, process.env.JWT_TOKEN_SECRET);
+        req.id = id;
+        next();
+    } catch (error) {
+        return next(new ErrorHandler('Invalid or expired token. Please login again.', 401));
+    }
 });
--- utils/SendToken.js
+++ utils/SendToken.js
@@ -1,17 +1,18 @@
 exports.sendtoken = (student, statusCode, res) => {
-	const token = student.getjwttoken();
+    const token = student.getjwttoken();
 
-	const options = {
-		expires: new Date(
-			Date.now() + process.env.COOKIE_EXPIRE * 1 * 60 * 60 * 1000
-		),
-		httpOnly: true,
-		// secure: true,
-	};
+    const options = {
+        expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 1 * 60 * 60 * 1000),
+        httpOnly: true,
+        sameSite: 'None', // Adjust this based on your requirements and browser support
+    };
 
-	res
-		.status(statusCode)
-		.cookie('token', token, options)
-		.json({ success: true, id: student._id,student:student, token });
+    if (process.env.isProtected === 'production') {
+        options.secure = true;
+    }
 
+    res
+        .status(statusCode)
+        .cookie('token', token, options)
+        .json({ success: true, id: student._id, student: student, token });
 };
